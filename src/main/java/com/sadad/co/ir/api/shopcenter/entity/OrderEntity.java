faa2a966e5c539b103263e50c6e822946bfe7860
M.Almasi <M.Almasi@sadad.co.ir>
2023-04-16 16:46:38 +0330

    add OrderEntity

--- src/main/java/com/sadad/co/ir/api/shopcenter/entity/OrderEntity.java
+++ src/main/java/com/sadad/co/ir/api/shopcenter/entity/OrderEntity.java
@@ -1,5 +1,29 @@
 package com.sadad.co.ir.api.shopcenter.entity;
 
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+import org.springframework.boot.autoconfigure.web.WebProperties;
 
+@Getter
+@Setter
+@Entity
+@Table(name = "order")
+// TODO: 4/16/2023 lombok
 public class OrderEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private int id;
+
+    @Column(name = "CUSTOMER_ID")
+    private int customerId;
+
+    @Column(name = "PAYMENT_ID",nullable = true)
+    private Integer paymentId;
+
+    @Column(name = "PRODUCT_ID",nullable = false)
+    private int productId;
+
+
 }
