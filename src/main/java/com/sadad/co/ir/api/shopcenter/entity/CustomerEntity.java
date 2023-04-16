a46b7142ed544e110c46c99a955d631f0d147a30
M.Almasi <M.Almasi@sadad.co.ir>
2023-04-16 16:53:48 +0330

    add CustomerEntity

--- src/main/java/com/sadad/co/ir/api/shopcenter/entity/CustomerEntity.java
+++ src/main/java/com/sadad/co/ir/api/shopcenter/entity/CustomerEntity.java
@@ -1,8 +1,24 @@
 package com.sadad.co.ir.api.shopcenter.entity;
 
+import jakarta.persistence.*;
 import lombok.Getter;
+import lombok.Setter;
 
 @Getter
-
+@Setter
+@Entity
+@Table(name = "CUSTOMER")
 public class CustomerEntity {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private int id;
+
+    @Column(name = "FIRST_NAME")
+    private String name;
+
+    @Column(name = "LAST_NAME")
+    private String lastName;
+
+    @Column(name = "ADDRESS")
+    private String address;
 }
