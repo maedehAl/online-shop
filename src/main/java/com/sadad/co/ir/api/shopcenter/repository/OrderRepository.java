de1fef614d0ebb798f7263f6deb987ec8ed8b817
M.Almasi <M.Almasi@sadad.co.ir>
2023-04-17 15:10:33 +0330

    add order REpository

--- src/main/java/com/sadad/co/ir/api/shopcenter/repository/OrderRepository.java
+++ src/main/java/com/sadad/co/ir/api/shopcenter/repository/OrderRepository.java
@@ -1,2 +1,9 @@
-package com.sadad.co.ir.api.shopcenter.repository;public interface OrderRepository {
+package com.sadad.co.ir.api.shopcenter.repository;
+
+import com.sadad.co.ir.api.shopcenter.entity.OrderEntity;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface OrderRepository extends JpaRepository<OrderEntity, Integer> {
 }
