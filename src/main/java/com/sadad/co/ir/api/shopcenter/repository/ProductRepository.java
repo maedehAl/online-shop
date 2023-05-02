634753719f821982abc865ddccb52896554e495d
M.Almasi <M.Almasi@sadad.co.ir>
2023-05-02 15:40:56 +0330

    pagination in product

--- src/main/java/com/sadad/co/ir/api/shopcenter/repository/ProductRepository.java
+++ src/main/java/com/sadad/co/ir/api/shopcenter/repository/ProductRepository.java
@@ -2,10 +2,15 @@ package com.sadad.co.ir.api.shopcenter.repository;
 
 import com.sadad.co.ir.api.shopcenter.entity.ProductEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
+
+import java.util.List;
 
 @Repository
 public interface ProductRepository extends JpaRepository<ProductEntity, Integer> {
 
+    //@Query("select p from ProductEntity p limit 5")
+    //List<ProductEntity> findAllTop();
 
 }
